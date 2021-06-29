95bc73fef068fb0edcf0da81dd538beccf2dae0d
YOUSSEF-MODS <86686274+YOUSSEF-MODS@users.noreply.github.com>
2021-06-29 12:39:45 -0700

    Create yemods.js

---
+++ yemods.js
@@ -0,0 +1,13 @@
+"version": "3.0",
+	"download": "https://ye-mods.blogspot.com/2021/05/yewhatsapp.html?m=1",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/Youssef-Ebrahim/wabuss/main/ye-update.html"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/Youssef-Ebrahim/wabuss/main/ye-Update-2.html"
+		}
+	]
+}
